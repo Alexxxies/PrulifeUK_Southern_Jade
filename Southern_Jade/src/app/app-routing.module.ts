09e4a0ed926ca324c3a8d44a43fc5e12267a17cd
allysa <allysajanecatibog@gmail.com>
2023-08-27 10:16:43 +0800

    Remove and re-add Southern_Jade directory

---
+++ Southern_Jade/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../Southern_Jade/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Southern_Jade/dist/index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
